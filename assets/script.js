05b426dd09596eafa945105723e97d65857038d5
Erin Platvoet <67918985+eplatvoet@users.noreply.github.com>
2024-08-13 15:58:36 -0400

    weather API calls working

--- assets/script.js
+++ assets/script.js
@@ -16,5 +16,44 @@ $(document).ready(function () {
     }
     setInterval(updateClock, 1000)
 
+    // FUNCTIONALITY FOR WEATHER BUTTON:
+    let zip = $("#city");
+
+    //USER ENTERS CITY AND PRESSES BUTTON:
+    $("#getInfo").click(function () {
+        zip = $("#city").val();
+        displayInformation();
+    });
+
+    function displayInformation() {
+        //INITIAL API CALL THAT WILL GRAB LAT/LON FROM RESULT
+        const apiKey = "940bd19264df2f0bdcef88196b007f5f";
+        const queryURL =
+        "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&APPID=" + apiKey + "&units=imperial";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            //ADDING CONTENT TO INDEX.HTML
+            $("#cityName").text(response.name);
+        
+            $("#temp").html(
+                "<img src='https://openweathermap.org/img/w/" +
+                response.weather[0].icon +
+                ".png'> <br>Current temperature:<br><span class='temperatures-from-api'>" +
+                response.main.temp +
+                "°</span> <br> Currently feels like:<br><span class='temperatures-from-api'>" +
+                response.main.feels_like +
+                "°</span> <br> Today's low:<br><span class='temperatures-from-api'>" + 
+                response.main.temp_min +
+                "°</span> <br> Today's high:<br><span class='temperatures-from-api'>" +
+                response.main.temp_max +
+                "°</span>"
+            );
+        });
+    }
+
+    
     
 });
