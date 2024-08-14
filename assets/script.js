e031fc428b10be0614b1032e947203a67d8ed5f7
Erin Platvoet <67918985+eplatvoet@users.noreply.github.com>
2024-08-14 13:38:09 -0400

    cleaning up js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,10 @@
 $(document).ready(function () {
-    console.log("Let's get this going")
-
     // LOAD DATE & TIME WHEN PAGE IS OPENED/REFRESHED
     // NOTE: USING MOMENT.JS WILL KEEP IT UPDATED WHILE PAGE IS OPEN
     $("#currentDay").text(moment().format('dddd'));  
     $("#currentDate").text(moment().format('l'));  
     $("#currentTime").text(moment().format('h:mm:ss a'));
   
-
     //FUNCTION FOR UPDATING TIME WITHOUT REFRESHING THE PAGE
     function updateClock() {
         $("#currentDay").text(moment().format('dddd'));  
@@ -18,25 +15,21 @@ $(document).ready(function () {
 
     // FUNCTIONALITY FOR WEATHER BUTTON:
     let zip = $("#city");
-
     //USER ENTERS CITY AND PRESSES BUTTON:
     $("#getInfo").click(function () {
         zip = $("#city").val();
         displayInformation();
     });
-
     function displayInformation() {
         //INITIAL API CALL THAT WILL GRAB LAT/LON FROM RESULT
         const apiKey = "940bd19264df2f0bdcef88196b007f5f";
         const queryURL =
         "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&APPID=" + apiKey + "&units=imperial";
-
         $.ajax({
             url: queryURL,
             method: "GET",
         }).then(function (response) {
             //ADDING CONTENT TO INDEX.HTML
-            // $("#cityName").text(response.name);
             $("#cityName").html(
                 response.name + 
                 "<img src='https://openweathermap.org/img/w/" +
@@ -56,14 +49,14 @@ $(document).ready(function () {
         });
     }
 
+    // TO DO LIST FUNCTIONALITY:
     // SAVING TO-DO LIST ITEMS FUNCTIONALITY:
-    //ONCE CLICKED, SAVE BUTTON WILL STORE THE USER'S INPUT TO LOCAL STORAGE SO THEY CAN VIEW ON THEIR CALENDAR IF PAGE GETS REFRESHED
+        // ONCE CLICKED, SAVE BUTTON WILL STORE THE USER'S INPUT TO LOCAL STORAGE SO THEY CAN VIEW ON THEIR CALENDAR IF PAGE GETS REFRESHED
     $(".saveBtn").on("click", function () {
         var value = $(this).siblings(".list-section").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, value);
     });
-
     //CODE TO RETRIEVE ITEMS FROM LOCAL STORAGE IF REFRESH HIT
     $("#hr-8 .list-section").val(localStorage.getItem("hr-8"));
     $("#hr-9 .list-section").val(localStorage.getItem("hr-9"));
@@ -76,12 +69,11 @@ $(document).ready(function () {
     $("#hr-16 .list-section").val(localStorage.getItem("hr-16"));
     $("#hr-17 .list-section").val(localStorage.getItem("hr-17"));
     $("#hr-18 .list-section").val(localStorage.getItem("hr-18"));
-
-    //VARIABLES SET UP TO DECLARE TIME TO BE USED IN FUNCTION BELOW
+    // FUNCTIONALITY TO DETERMINE PAST/PRESENT/FUTURE
+    // VARIABLES SET UP TO DECLARE TIME TO BE USED IN FUNCTION BELOW
     var today = new Date();
     let currentTime = today.getHours();
-    
-    //FUNCTION CREATED TO SHOW A DIFFERENT COLOR BACKGROUND DEPENDING ON THE CURRENT TIME OF DAY
+    // FUNCTION CREATED TO SHOW A DIFFERENT COLOR BACKGROUND DEPENDING ON THE CURRENT TIME OF DAY
     function updateColor() {
         // let currentTime = today.getHours();
         $(".timeslot").each(function () {
@@ -95,8 +87,6 @@ $(document).ready(function () {
             }
         })
     };
-
     //FUNCTION CALL TO GET THE COLORS TO UPDATE:
     updateColor();
-    
 });
